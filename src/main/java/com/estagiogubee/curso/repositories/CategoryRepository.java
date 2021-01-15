6ffb539cad7d33e436d55b064780bb558dfd5ecb
MucaBordini <mucabordini@gmail.com>
2021-01-15 09:23:42 -0300

    Category

---
+++ src/main/java/com/estagiogubee/curso/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.estagiogubee.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estagiogubee.curso.entities.Category;
+
+public interface CategoryRepository  extends JpaRepository<Category, Long>{
+
+}
